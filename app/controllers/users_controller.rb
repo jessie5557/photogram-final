92a7d1f110ba0b630917cd663047b85b079e1c67
jessie5557 <ychen38@chicagobooth.edu>
2025-03-11 06:28:03 +0000

    users/username

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,13 @@
 class UsersController < ApplicationController
   def index
     matching_users = User.all
-
     @list_of_users = matching_users.order({ :username => :asc })
-
     render({ :template => "users/index" })
   end
+
+  def show
+    @the_username = params.fetch("username")
+    @matching_user = User.where({ :username => @the_username }).at(0)
+  end
+
 end
