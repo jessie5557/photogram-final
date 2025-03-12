89c4125fd86f3bc2c54b177ba55266fb0408c9e1
jessie5557 <ychen38@chicagobooth.edu>
2025-03-12 06:19:27 +0000

    home works

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 Rails.application.routes.draw do
 
+  devise_for :users
+  root to: "users#index"
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  # root "articles#index"
+
   get("/", { :controller => "users", :action => "index"})
   get("/users", { :controller => "users", :action => "index"})
   get("/users/:username", { :controller => "users", :action => "show"})
@@ -81,10 +88,5 @@ Rails.application.routes.draw do
 
   #------------------------------
 
-  devise_for :users
-  root to: "users#index"
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  # root "articles#index"
 end
